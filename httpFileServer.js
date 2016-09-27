4a2f1eef2a15dbc8058085383c12e3efce43ac7c
Vitor <vitormd97@gmail.com>
2016-09-26 21:39:44 -0300

    httpFileServer comments added

--- httpFileServer.js
+++ httpFileServer.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (request, response) {
+  //response.writeHead(200, { 'content-type': 'text/plain' }); to format response 
   fs.createReadStream(process.argv[3]).pipe(response);
 });
-server.listen(process.argv[2]);
+server.listen(process.argv[2]); //cast to number
